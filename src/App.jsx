48b2db265fc8ef1357930990c889c484fcd2e931
Spacs21 <behaksr123@gmail.com>
2024-10-25 09:18:12 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import Category from './components/category/Category'
+import Products from './components/products/Products'
+import About from './components/about/About'
+import Subscribe from './components/subscribe/Subscribe'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <div className="color">
+      <Header/>
+      <Hero/>
+    </div>
+    <Category/>
+    <Products/>
+    <About/>
+    <Subscribe/>
+    <Footer/>
     </>
   )
 }
